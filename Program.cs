f8894030e68854bf0633a99bee2d61fd54da6442
Michael Maragos <maragosm@aol.com>
2022-06-27 10:59:02 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var favGames = new List<string>();
+
+            favGames.Add("DeepRock Galactic");
+            favGames.Add("Warframe");
+            favGames.Add("Minecraft");
+            favGames.Add("Terraria");
+            favGames.Add("Planetside 2");
+            favGames.Add("Valheim");
+            favGames.Add("7 Days to Die");
+            favGames.Add("Subnautica");
+
+            var gamesListOrdered = favGames.OrderBy(name => name.Length).ToList();
+
+            foreach (var game in gamesListOrdered)
+            {
+                Console.WriteLine(game);
+            }
+        }
+    }
+}
